cd1bc77cffef75de1b7ca4e085569ce9ecc8315f
cvicens <carlos.vicens.alonso@gmail.com>
2024-11-06 10:08:41 +0100

    accelerator fixed

--- gitops/embeddings/run.sh
+++ gitops/embeddings/run.sh
@@ -2,17 +2,19 @@
 ARGOCD_APP_NAME=vllm-mistral-7b
 
 # Load environment variables
-DATA_SCIENCE_PROJECT_NAMESPACE="vllm-mistral-7b-instruct-v0.2"
+DATA_SCIENCE_PROJECT_NAMESPACE="embeddings"
+
+  # --set model.accelerator.productName="NVIDIA-A10G" \
+  # --set model.accelerator.min=1 \
+  # --set model.accelerator.max=1 \
 
 helm template . --name-template ${ARGOCD_APP_NAME} \
-  --set instanceName="vllm-mistral-7b" \
+  --set createNamespace="true" \
+  --set createSecret="true" \
   --set dataScienceProjectNamespace=${DATA_SCIENCE_PROJECT_NAMESPACE} \
   --set dataScienceProjectDisplayName=${DATA_SCIENCE_PROJECT_NAMESPACE} \
-  --set model.root=mistralai \
-  --set model.id=Mistral-7B-Instruct-v0.2 \
-  --set model.name=mistral-7b \
-  --set model.displayName="Mistral 7b" \
-  --set model.accelerator.productName="NVIDIA-A10G" \
-  --set model.accelerator.min=1 \
-  --set model.accelerator.max=1 \
+  --set model.root=nomic-ai \
+  --set model.id=nomic-embed-text \
+  --set model.name=nomic-embed \
+  --set model.displayName="Nomic AI Tesxt" \
   --include-crds
